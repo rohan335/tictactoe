1c637dd2b331d00396e624b85893e7af123e633a
Rohan <rohandesai335@gmail.com>
2021-09-13 22:38:21 -0400

    finished beta

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Minimax from 'tic-tac-toe-minimax'
+import Modal from 'react-native-modal';
 import { Square } from './Square'
+import {useFonts} from 'expo-font'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function App() {
 
@@ -23,23 +26,64 @@ export default function App() {
     winner: null,
     board: [0,1,2,3,4,5,6,7,8]
   })
+
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
+  const [isDraw, setIsDraw] = useState(true)
   
   //const nextMove = ComputerMove( board, symbols, difficulty );
 
   //handlers
-  const clickHandler = squareNum => {    
+  const clickHandler = squareNum => {  
+    if (typeof board.board[squareNum] == 'string') {
+      return;
+    }
     let newBoard = board.board
     newBoard[squareNum] = huPlayer
-    const humanStep = GameStep( newBoard, symbols, difficulty );
-    setBoard(humanStep)
+    setBoard({
+      board: newBoard,
+      winner: null
+    })
 
-    //const nextMove = ComputerMove( board.board, symbols, difficulty );
     newBoard = board.board
-    //newBoard[nextMove] = aiPlayer
-    const aiStep = GameStep( newBoard, symbols, difficulty );
-    setBoard(aiStep)
+    setBoard(GameStep( newBoard, symbols, difficulty ))
+    
+  }
 
-    console.log(board)
+  //console.log(board.winner)
+  /*switch (board.winner) {
+    case "aiPlayer":
+      conso
+      () => setIsModalVisible(true)
+      break;
+    case "huPlayer":
+      () => setIsModalVisible(true)
+      break;
+    case "draw": 
+      () => setIsModalVisible(true)
+      break;
+    default:
+      break;
+  }*/
+
+  useEffect(
+    () => {
+      if(board.winner == "aiPlayer") {
+        setIsDraw(false)
+        setIsModalVisible(true)
+      } else if (board.winner == "draw") {
+        setIsDraw(true)
+        setIsModalVisible(true)
+      }
+    }, [board]
+  )
+
+  const [loaded] = useFonts({
+    InconsolataM: require('./Inconsolata-Medium.ttf')
+  });
+
+  if(!loaded) {
+      return null;
   }
 
   return (
@@ -92,6 +136,31 @@ export default function App() {
         </View>
 
       </View>
+
+      <Modal isVisible={isModalVisible} onBackdropPress={() => {
+          setBoard({
+            winner: null,
+            board: [0,1,2,3,4,5,6,7,8]
+          })
+          setIsModalVisible(false)
+        }}>
+        <View style={{ width: '80%', backgroundColor: '#222222', alignSelf: 'center', borderRadius: 14, padding: '10%'}}>
+          <Text style={{color: '#fff', alignSelf: 'center', fontSize: 25, fontFamily: 'InconsolataM'}}>{isDraw ? "Tie game!" : "You lost!"}</Text>
+          <Text style={{color: '#fff', alignSelf: 'center', fontSize: 20, marginTop: '4%', fontFamily: 'InconsolataM'}}>{isDraw ? "So close!" : "Better luck next time!"}</Text>
+          <TouchableOpacity activeOpacity={0.9} style={{ width: '80%', justifyContent: 'center', height: 45, backgroundColor: '#F1F1F1', alignSelf: 'center', marginTop: '30%', marginBottom: '20%'}} onPress={() => {
+          setBoard({
+            winner: null,
+            board: [0,1,2,3,4,5,6,7,8]
+          })
+          setIsModalVisible(false)
+        }}>
+            <View style={{flexDirection: 'row', alignSelf: 'center'}}>
+              <Ionicons name="arrow-redo" size={18}></Ionicons>
+              <Text style={{fontFamily: 'InconsolataM', fontSize: 18, marginLeft: '5%'}}>Rematch?</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </Modal>
       
       <StatusBar style="auto" />
     </View>
--- Square.js
+++ Square.js
@@ -1,15 +1,37 @@
 import { setStatusBarNetworkActivityIndicatorVisible, StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Animated } from 'react-native';
 
 export const Square = ({squareState, onPress, pos}) => {
 
     const [player, setPlayer] = useState("")
 
+    const [textSize] = useState(new Animated.Value(0))
+
+    const startAnimationLike = () => {
+        Animated.spring(textSize, {
+            toValue: 1,
+            bounciness: 15,
+            useNativeDriver: true
+        }).start()
+    }
+
+    const startAnimationUnlike = () => {
+        Animated.timing(textSize, {
+            toValue: 0,
+            duration: 400,
+            useNativeDriver: true
+        }).start()
+    }
+
     useEffect(
         () => {
             if(typeof squareState.board[pos] == 'string') {
                 setPlayer(squareState.board[pos])
+                startAnimationLike()
+            } else {
+                setPlayer("")
+                startAnimationUnlike()
             }
         }
     )
@@ -19,7 +41,14 @@ export const Square = ({squareState, onPress, pos}) => {
             onPress()
         }}>
             <View style={{ height: 120, width: 120, backgroundColor: '#373737', borderRadius: 10, alignItems: 'center', justifyContent: 'center'}}>
-                <Text style={{color: '#fff', fontSize: 20}}>{player}</Text>
+                    <Animated.View style={{transform: [{scale: textSize.interpolate(
+                        {
+                            inputRange: [0, 0.5, 1],
+                            outputRange: [1, 1.5, 1]
+                        }
+                    )}]}}>
+                        <Animated.Text style={{color: '#fff', fontSize: 20}}>{player}</Animated.Text>
+                    </Animated.View>
             </View>
         </TouchableOpacity>
     )
